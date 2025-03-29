cba81428faea53058c0c156ebf762e90d5f6741d
Nripendra101 <nripendra972000@gmail.com>
2025-03-30 00:41:17 +0530

    updated nripendra-test2

--- nripendra-test2/lambda_function.py
+++ nripendra-test2/lambda_function.py
@@ -6,3 +6,10 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('now these are in nripendra-test2 function!')
     }
+    # Add a print statement to check if the function is being called
+    print("Lambda function called")
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Lambda function called successfully')
+    }
+  
